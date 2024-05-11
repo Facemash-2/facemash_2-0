ffd15831fd10a20fea11362058084c1c18281cdb
Facemash-2 <784a35689@gmail.com>
2024-05-11 22:37:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,14 +33,17 @@ def calculate_expected_outcome(rating_a, rating_b):
 
 @app.route('/results')
 def results():
-    # Retrieve the top 3 candidates from the leaderboard
-    top_candidates = mongo.db.votes.find().sort([("score", -1)]).limit(3)
+    try:
+        # Retrieve the top 3 candidates from the leaderboard
+        top_candidates = mongo.db.votes.find().sort([("score", -1)]).limit(3)
 
-    # Render the results.html template with the top 3 candidates
-    return render_template('results.html', 
-                           top_candidate=top_candidates[0],
-                           second_candidate=top_candidates[1],
-                           third_candidate=top_candidates[2])
+        # Render the results.html template with the top 3 candidates
+        return render_template('results.html', 
+                               top_candidate=top_candidates[0],
+                               second_candidate=top_candidates[1],
+                               third_candidate=top_candidates[2])
+    except Exception as e:
+        return f"An error occurred: {e}"
 
 @app.route('/admin')
 def admin():
